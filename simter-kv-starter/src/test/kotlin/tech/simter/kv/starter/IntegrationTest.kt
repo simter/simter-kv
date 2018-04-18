56f7489551ffa89b758eec1d1425535fc4c967b9
Unknown <rongjihuang@gmail.com>
2018-04-18 23:35:19 +0800

    Polishing IntegrationTest

---
+++ simter-kv-starter/src/test/kotlin/tech/simter/kv/starter/IntegrationTest.kt
@@ -0,0 +1,92 @@
+package tech.simter.kv.starter
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
+import java.util.*
+import javax.json.Json
+
+/**
+ * 实际环境的集成测试。
+ *
+ * see [WebClientIntegrationTests](https://github.com/spring-projects/spring-framework/blob/v5.0.5.RELEASE/spring-webflux/src/test/java/org/springframework/web/reactive/function/client/WebClientIntegrationTests.java)
+ *
+ * @author RJ
+ */
+@Disabled
+class IntegrationTest {
+  private val webClient = WebTestClient.bindToServer().baseUrl("http://localhost:8085").build()
+  private val contextPath = ""
+
+  @Test
+  fun findOne() {
+    // prepare data
+    val key = UUID.randomUUID().toString()
+    val value = "v"
+    val map = mapOf(key to value)
+    save(map)
+
+    // find
+    webClient.get().uri("$contextPath/$key")
+      .exchange()
+      .expectStatus().isOk
+      .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+      .expectBody()
+      .json(map2JsonString(map))
+  }
+
+  @Test
+  fun findOneButNotFound() {
+    val key = UUID.randomUUID().toString()
+    webClient.get().uri("$contextPath/$key")
+      .exchange()
+      .expectStatus().isNoContent
+  }
+
+  @Test
+  fun findMulti() {
+    // prepare data
+    val map = (1..3).associate { "k-$it" to "v-$it" }
+    save(map)
+
+    // find
+    webClient.get().uri("$contextPath/" + map.keys.joinToString(","))
+      .exchange()
+      .expectStatus().isOk
+      .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+      .expectBody()
+      .json(map2JsonString(map))
+  }
+
+  @Test
+  fun saveNone() {
+    save(mapOf())
+  }
+
+  @Test
+  fun saveOne() {
+    save(mapOf(UUID.randomUUID().toString() to "v"))
+  }
+
+  @Test
+  fun saveMulti() {
+    save((1..3).associate { "k-$it" to "v-0-$it" })
+  }
+
+  private fun save(map: Map<String, String>) {
+    webClient.post().uri(contextPath)
+      .contentType(MediaType.APPLICATION_JSON_UTF8)
+      .syncBody(map2JsonString(map))
+      .exchange()
+      .expectStatus().isNoContent
+  }
+
+  private fun map2JsonString(map: Map<String, String>): String {
+    return if (map.isEmpty()) "{}" else {
+      val json = Json.createObjectBuilder()
+      map.forEach { json.add(it.key, it.value) }
+      json.build().toString()
+    }
+  }
+}
