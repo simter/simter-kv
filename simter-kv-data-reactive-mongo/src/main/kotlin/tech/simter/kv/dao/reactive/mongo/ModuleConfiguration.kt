0bc49cb3918dea02bb65f642e0c89a758b336a21
RJ.Hwang <rongjihuang@gmail.com>
2018-04-19 17:14:53 +0800

    Polishing

--- simter-kv-data-reactive-mongo/src/main/kotlin/tech/simter/kv/dao/reactive/mongo/ModuleConfiguration.kt
+++ simter-kv-data-reactive-mongo/src/main/kotlin/tech/simter/kv/dao/reactive/mongo/ModuleConfiguration.kt
@@ -1,8 +1,13 @@
 package tech.simter.kv.dao.reactive.mongo
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.core.mapping.event.LoggingEventListener
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
 private const val MODULE_PACKAGE = "tech.simter.kv.dao.reactive.mongo"
@@ -17,12 +22,12 @@ private const val MODULE_PACKAGE = "tech.simter.kv.dao.reactive.mongo"
 @ComponentScan(MODULE_PACKAGE)
 @EntityScan(basePackages = ["tech.simter.kv.po"])
 class ModuleConfiguration {
-//  private val logger = LoggerFactory.getLogger(ModuleConfiguration::class.java)
-//  @Bean
-//  @ConditionalOnProperty(name = ["simter.mongodb.enabled-logging-event-listener"], havingValue = "true")
-//  @ConditionalOnMissingBean
-//  fun mongoEventListener(): LoggingEventListener {
-//    logger.warn("instance a LoggingEventListener bean for mongodb operations")
-//    return LoggingEventListener()
-//  }
+  private val logger = LoggerFactory.getLogger(ModuleConfiguration::class.java)
+  @Bean
+  @ConditionalOnProperty(name = ["simter.mongodb.enabled-logging-event-listener"], havingValue = "true")
+  @ConditionalOnMissingBean
+  fun mongoEventListener(): LoggingEventListener {
+    logger.warn("instance a LoggingEventListener bean for mongodb operations")
+    return LoggingEventListener()
+  }
 }
