5a4b732c92e776b2eb081b25b2683263281d2bc3
RJ <rongjihuang@gmail.com>
2019-03-27 21:39:44 +0800

    Globally quoted for mysql
    1. [MySQL Keywords and Reserved Words](https://dev.mysql.com/doc/refman/8.0/en/keywords.html)
    2. [Global escaping using the Hibernate globally_quoted_identifiers property](https://vladmihalcea.com/escape-sql-reserved-keywords-jpa-hibernate/)

--- simter-kv-data/src/main/resources/tech/simter/kv/sql/mysql/schema-create.sql
+++ simter-kv-data/src/main/resources/tech/simter/kv/sql/mysql/schema-create.sql
@@ -2,7 +2,7 @@
  * Create table script.
  * @author RJ
  */
-create table st_kv (
+create table `st_kv` (
   `key`   varchar(100) not null comment 'Key',
   `value` text not null comment 'Value',
   primary key (`key`)
