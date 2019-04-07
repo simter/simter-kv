fcf791f356837c3a587856fe5f6ec471ba24c0d0
RJ <rongjihuang@gmail.com>
2019-04-07 22:43:46 +0800

    data-jpa - fixed unit test

--- simter-kv-data-jpa/src/test/kotlin/tech/simter/kv/dao/jpa/DeleteMethodImplTest.kt
+++ simter-kv-data-jpa/src/test/kotlin/tech/simter/kv/dao/jpa/DeleteMethodImplTest.kt
@@ -1,24 +1,22 @@
 package tech.simter.kv.dao.jpa
 
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import reactor.test.test
 import tech.simter.kv.dao.KeyValueDao
 import tech.simter.kv.po.KeyValue
+import tech.simter.reactive.jpa.ReactiveEntityManager
+import tech.simter.reactive.test.jpa.ReactiveDataJpaTest
 import java.util.*
-import javax.persistence.EntityManager
-import javax.persistence.PersistenceContext
 
 /**
  * @author RJ
  */
 @SpringJUnitConfig(UnitTestConfiguration::class)
-@DataJpaTest
+@ReactiveDataJpaTest
 class DeleteMethodImplTest @Autowired constructor(
-  @PersistenceContext val em: EntityManager,
+  val rem: ReactiveEntityManager,
   val dao: KeyValueDao
 ) {
   @Test
@@ -34,19 +32,19 @@ class DeleteMethodImplTest @Autowired constructor(
   @Test
   fun `delete exists key`() {
     // prepare data
-    val pos = (1..3).map { KeyValue("k-$it", "v-$it") }
-    pos.forEach { em.persist(it) }
-    em.flush()
-    em.clear()
+    val random = UUID.randomUUID().toString();
+    val pos = (1..3).map { KeyValue("$random-$it", "$random-$it") }
+    rem.persist(*pos.toTypedArray()).test().verifyComplete()
 
-    // verify empty result
+    // do delete
     dao.delete(*pos.map { it.key }.toTypedArray()).test().verifyComplete()
 
     // verify deleted
-    assertEquals(0L,
-      em.createQuery("select count(key) from KeyValue where key in (:keys)")
-        .setParameter("keys", pos.map { it.key })
-        .singleResult
-    )
+    rem.createQuery("select count(key) from KeyValue where key in (:keys)", Long::class.javaObjectType)
+      .setParameter("keys", pos.map { it.key })
+      .singleResult
+      .test()
+      .expectNext(0L)
+      .verifyComplete()
   }
 }
--- simter-kv-data-jpa/src/test/kotlin/tech/simter/kv/dao/jpa/SaveMethodImplTest.kt
+++ simter-kv-data-jpa/src/test/kotlin/tech/simter/kv/dao/jpa/SaveMethodImplTest.kt
@@ -1,24 +1,22 @@
 package tech.simter.kv.dao.jpa
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import reactor.test.test
 import tech.simter.kv.dao.KeyValueDao
 import tech.simter.kv.po.KeyValue
+import tech.simter.reactive.jpa.ReactiveEntityManager
+import tech.simter.reactive.test.jpa.ReactiveDataJpaTest
 import java.util.*
-import javax.persistence.EntityManager
-import javax.persistence.PersistenceContext
 
 /**
  * @author RJ
  */
 @SpringJUnitConfig(UnitTestConfiguration::class)
-@DataJpaTest
+@ReactiveDataJpaTest
 class SaveMethodImplTest @Autowired constructor(
-  @PersistenceContext val em: EntityManager,
+  val rem: ReactiveEntityManager,
   val dao: KeyValueDao
 ) {
   @Test
@@ -29,29 +27,34 @@ class SaveMethodImplTest @Autowired constructor(
 
   @Test
   fun `save one`() {
+    // do save
     val po = KeyValue(UUID.randomUUID().toString(), "v")
-    dao.save(mapOf(po.key to po.value)).test().expectNextCount(0L).verifyComplete()
+    dao.save(mapOf(po.key to po.value)).test().verifyComplete()
 
     // verify saved
-    assertThat(
-      em.createQuery("select a from KeyValue a where key = :key", KeyValue::class.java)
-        .setParameter("key", po.key).singleResult
-    ).isEqualTo(po)
+    rem.createQuery("select a from KeyValue a where key = :key", KeyValue::class.java)
+      .setParameter("key", po.key)
+      .singleResult
+      .test()
+      .expectNext(po)
+      .verifyComplete()
   }
 
   @Test
   fun `save many`() {
-    val pos = (1..3).map { KeyValue("k-$it", "v-$it") }
-
-    // verify result
+    // do save
+    val random = UUID.randomUUID().toString();
+    val pos = (1..3).map { KeyValue("$random-$it", "$random-$it") }
     dao.save(pos.associate { it.key to it.value }).test().expectNextCount(0L).verifyComplete()
 
     // verify saved
     pos.forEach {
-      assertThat(
-        em.createQuery("select a from KeyValue a where key = :key", KeyValue::class.java)
-          .setParameter("key", it.key).singleResult
-      ).isEqualTo(it)
+      rem.createQuery("select a from KeyValue a where key = :key", KeyValue::class.java)
+        .setParameter("key", it.key)
+        .singleResult
+        .test()
+        .expectNext(it)
+        .verifyComplete()
     }
   }
 }
