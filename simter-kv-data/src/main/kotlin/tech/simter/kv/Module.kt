6ebadfbe068e917493c71479f2175140f20811f6
RJ <rongjihuang@gmail.com>
2019-04-11 18:30:54 +0800

    Rename TABLE_NAME to TABLE_KV

--- simter-kv-data/src/main/kotlin/tech/simter/kv/Module.kt
+++ simter-kv-data/src/main/kotlin/tech/simter/kv/Module.kt
@@ -1,12 +1,10 @@
 package tech.simter.kv
 
-import tech.simter.kv.po.KeyValue
-
 /** The module's package name */
 const val PACKAGE_NAME = "tech.simter.kv"
 
-/** The [KeyValue] entity's table name */
-const val TABLE_NAME = "st_kv"
+/** Entities table name */
+const val TABLE_KV = "st_kv"
 
 /** The operation name */
 const val OPERATION_READ = "READ"
--- simter-kv-data/src/main/kotlin/tech/simter/kv/po/KeyValue.kt
+++ simter-kv-data/src/main/kotlin/tech/simter/kv/po/KeyValue.kt
@@ -1,7 +1,7 @@
 package tech.simter.kv.po
 
 import org.springframework.data.mongodb.core.mapping.Document
-import tech.simter.kv.TABLE_NAME
+import tech.simter.kv.TABLE_KV
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Table
@@ -12,8 +12,8 @@ import javax.persistence.Table
  * @author RJ
  */
 @Entity
-@Table(name = TABLE_NAME)
-@Document(collection = TABLE_NAME)
+@Table(name = TABLE_KV)
+@Document(collection = TABLE_KV)
 data class KeyValue(
   @javax.persistence.Id
   @org.springframework.data.annotation.Id
