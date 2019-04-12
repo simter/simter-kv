58badf1fec44853451d9c49fb1a0f1ab915152e4
RJ <rongjihuang@gmail.com>
2019-04-12 11:50:41 +0800

    Support static file on starter
    Resolved https://github.com/simter/simter-kv/issues/10

--- simter-kv-starter/src/main/kotlin/tech/simter/kv/starter/AppConfiguration.kt
+++ simter-kv-starter/src/main/kotlin/tech/simter/kv/starter/AppConfiguration.kt
@@ -4,14 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.config.CorsRegistry
-import org.springframework.web.reactive.config.DelegatingWebFluxConfiguration
-import org.springframework.web.reactive.config.EnableWebFlux
-import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.http.CacheControl
+import org.springframework.http.HttpHeaders.ORIGIN
+import org.springframework.web.cors.reactive.CorsUtils
+import org.springframework.web.reactive.config.*
 import org.springframework.web.reactive.function.server.router
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
 import tech.simter.kv.PACKAGE
 import tech.simter.reactive.web.Utils.TEXT_HTML_UTF8
 import java.time.OffsetDateTime
+import java.util.concurrent.TimeUnit
 
 /**
  * Application WebFlux Configuration.
@@ -49,6 +53,13 @@ class AppConfiguration @Autowired constructor(
           .allowCredentials(false)
           .maxAge(1800) // seconds
       }
+
+      /** See [Static Resources](https://docs.spring.io/spring/docs/current/spring-framework-reference/web-reactive.html#webflux-config-static-resources) */
+      override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/static/**")
+          .addResourceLocations("classpath:/META-INF/resources/static/")
+          .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+      }
     }
   }
 
@@ -65,5 +76,24 @@ class AppConfiguration @Autowired constructor(
   @Bean
   fun rootRoutes() = router {
     "/".nest { GET("/") { ok().contentType(TEXT_HTML_UTF8).syncBody(rootPage) } }
+  }
+
+  /**
+   * Enabled static file for CORS request.
+   *
+   * Just add Access-Control-Allow-Origin header.
+   */
+  @Bean
+  fun corsFilter4StaticFile(): WebFilter {
+    return WebFilter { exchange: ServerWebExchange, chain: WebFilterChain ->
+      val request = exchange.request
+      if (CorsUtils.isCorsRequest(request)                          // cross origin
+        && !CorsUtils.isPreFlightRequest(request)                   // not OPTION request
+        && request.path.value().startsWith("/static/")) {   // only for static file dir
+        // Add Access-Control-Allow-Origin header
+        exchange.response.headers.add("Access-Control-Allow-Origin", request.headers.getFirst(ORIGIN))
+      }
+      chain.filter(exchange)
+    }
   }
 }
