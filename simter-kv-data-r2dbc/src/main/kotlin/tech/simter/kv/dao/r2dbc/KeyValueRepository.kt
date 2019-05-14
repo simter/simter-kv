64ec7f27f978eaeefcca5e32f8d7dcd8c7e9924d
RJ <rongjihuang@gmail.com>
2019-05-14 22:53:29 +0800

    Fixed compile error after upgrade to spring-data-r2dbc-1.0.0.M2

--- simter-kv-data-r2dbc/src/main/kotlin/tech/simter/kv/dao/r2dbc/KeyValueRepository.kt
+++ simter-kv-data-r2dbc/src/main/kotlin/tech/simter/kv/dao/r2dbc/KeyValueRepository.kt
@@ -1,6 +1,6 @@
 package tech.simter.kv.dao.r2dbc
 
-import org.springframework.data.r2dbc.function.DatabaseClient
+import org.springframework.data.r2dbc.core.DatabaseClient
 import org.springframework.data.r2dbc.repository.support.SimpleR2dbcRepository
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
