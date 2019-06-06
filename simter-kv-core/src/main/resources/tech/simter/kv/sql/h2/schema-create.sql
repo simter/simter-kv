ccba570c77f8d9f3f5a1e3c5b5b429c2470577b8
RJ <rongjihuang@gmail.com>
2019-06-06 13:43:11 +0800

    Use varchar(4000)  instead of text on h2:st_kv.v
    Because r2dbc-h2 could not decode text column to String directly. It consider text column as a Clob column

--- simter-kv-core/src/main/resources/tech/simter/kv/sql/h2/schema-create.sql
+++ simter-kv-core/src/main/resources/tech/simter/kv/sql/h2/schema-create.sql
@@ -1,6 +1,6 @@
 create table st_kv (
   k varchar(100) primary key,
-  v text not null
+  v varchar(4000) not null
 );
 comment on table st_kv is 'Key-Value Pair';
 comment on column st_kv.k is 'Key';
