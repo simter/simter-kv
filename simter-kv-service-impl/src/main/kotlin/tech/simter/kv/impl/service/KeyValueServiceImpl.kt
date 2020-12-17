a3fdfac7be7bd592e81d7d1cb50d5ca469c155ad
RJ <rongjihuang@gmail.com>
2020-12-17 15:19:00 +0800

    Add `@Transactional` config on `KeyValueServiceImpl`

--- simter-kv-service-impl/src/main/kotlin/tech/simter/kv/impl/service/KeyValueServiceImpl.kt
+++ simter-kv-service-impl/src/main/kotlin/tech/simter/kv/impl/service/KeyValueServiceImpl.kt
@@ -3,6 +3,7 @@ package tech.simter.kv.impl.service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Mono
 import tech.simter.kv.AUTHORIZER_KEY
 import tech.simter.kv.OPERATION_DELETE
@@ -23,21 +24,25 @@ class KeyValueServiceImpl @Autowired constructor(
   private val moduleAuthorizer: ModuleAuthorizer,
   private val dao: KeyValueDao
 ) : KeyValueService {
+  @Transactional(readOnly = true)
   override fun valueOf(key: String): Mono<String> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_READ)
       .then(Mono.defer { dao.valueOf(key) })
   }
 
+  @Transactional(readOnly = true)
   override fun find(vararg keys: String): Mono<Map<String, String>> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_READ)
       .then(Mono.defer { dao.find(*keys) })
   }
 
+  @Transactional(readOnly = false)
   override fun save(keyValues: Map<String, String>): Mono<Void> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_SAVE)
       .then(Mono.defer { dao.save(keyValues) })
   }
 
+  @Transactional(readOnly = false)
   override fun delete(vararg keys: String): Mono<Void> {
     return moduleAuthorizer.verifyHasPermission(OPERATION_DELETE)
       .then(Mono.defer { dao.delete(*keys) })
