267f2d4a700dfc7a576987c8288bbe8b196e8a4e
RJ <rongjihuang@gmail.com>
2021-04-16 16:36:28 +0800

    Implement fuzzy search dao method by r2dbc

--- simter-kv-dao-r2dbc/src/main/kotlin/tech/simter/kv/impl/dao/r2dbc/KeyValueDaoImpl.kt
+++ simter-kv-dao-r2dbc/src/main/kotlin/tech/simter/kv/impl/dao/r2dbc/KeyValueDaoImpl.kt
@@ -20,24 +20,49 @@ class KeyValueDaoImpl @Autowired constructor(
   override fun valueOf(key: String): Mono<String> {
     return databaseClient.sql("select v from $TABLE_KV where k = :key")
       .bind("key", key)
-      .map { row -> row.get("v", String::class.java) }
+      .map { row -> row.get("v", String::class.java)!! }
       .one()
   }
 
   override fun find(vararg keys: String): Mono<Map<String, String>> {
     return if (keys.isEmpty()) Mono.empty()
-    else (
-      if (keys.size == 1)
-        databaseClient.sql("select k, v from $TABLE_KV where k = :key")
-          .bind("key", keys[0])
-      else
-        databaseClient.sql("select k, v from $TABLE_KV where k in (:keys)")
-          .bind("keys", keys.toList())
-      )
-      .map { row -> arrayOf(row.get("k", String::class.java), row.get("v", String::class.java)) }
-      .all()
-      .collectMap({ it[0] }, { it[1] })
-      .flatMap { if (it.isEmpty()) Mono.empty() else Mono.just(it) }
+    else {
+      if (keys.size == 1) { // single key
+        if (keys[0].contains("%")) { // fuzzy match
+          databaseClient.sql("select k, v from $TABLE_KV where k like :key order by k asc")
+            .bind("key", keys[0])
+        } else { // equation match
+          databaseClient.sql("select k, v from $TABLE_KV where k = :key")
+            .bind("key", keys[0])
+        }
+      } else { // multiple keys
+        val equationMatchKeys = keys.filterNot { it.contains("%") }
+        val fuzzyMatchKeys = keys.filter { it.contains("%") }
+        val fuzzyMatchConditions = mutableListOf<String>()
+        fuzzyMatchKeys.forEachIndexed { index, _ -> fuzzyMatchConditions.add("k like :key$index") }
+
+        // build sql
+        var sql = "select k, v from $TABLE_KV where "
+        sql += if (equationMatchKeys.isEmpty()) {
+          fuzzyMatchConditions.joinToString(" or ")
+        } else {
+          "k in (:keys)" + if (fuzzyMatchConditions.isEmpty()) "" else (" or " + fuzzyMatchConditions.joinToString(" or "))
+        }
+        sql += " order by k asc"
+        println(sql)
+        var spec = databaseClient.sql(sql)
+
+        // bind params
+        if (equationMatchKeys.isNotEmpty()) spec = spec.bind("keys", equationMatchKeys)
+        fuzzyMatchKeys.forEachIndexed { index, key -> spec = spec.bind("key$index", key) }
+
+        // return
+        spec
+      }.map { row -> arrayOf(row.get("k", String::class.java)!!, row.get("v", String::class.java)!!) }
+        .all()
+        .collectMap({ it[0] }, { it[1] })
+        .flatMap { if (it.isEmpty()) Mono.empty() else Mono.just(it) }
+    }
   }
 
   override fun save(keyValues: Map<String, String>): Mono<Void> {
--- simter-kv-dao-r2dbc/src/test/kotlin/tech/simter/kv/impl/dao/r2dbc/FindMethodImplTest.kt
+++ simter-kv-dao-r2dbc/src/test/kotlin/tech/simter/kv/impl/dao/r2dbc/FindMethodImplTest.kt
@@ -1,13 +1,18 @@
 package tech.simter.kv.impl.dao.r2dbc
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import reactor.kotlin.test.test
 import tech.simter.kv.core.KeyValueDao
+import tech.simter.kv.impl.dao.r2dbc.TestHelper.randomKeyValuePo
+import tech.simter.kv.impl.dao.r2dbc.po.KeyValuePo
 import tech.simter.kv.test.TestHelper.randomKey
+import tech.simter.kv.test.TestHelper.randomValue
+import tech.simter.util.RandomUtils.randomString
 
 /**
  * Test [KeyValueDaoImpl.find].
@@ -32,16 +37,114 @@ class FindMethodImplTest @Autowired constructor(
   }
 
   @Test
-  fun `find exists key`() {
+  fun `find single key by equation match`() {
     // prepare data
-    val pos = (1..3).map { repository.save(TestHelper.randomKeyValuePo()).block()!! }
+    val po = repository.save(randomKeyValuePo()).block()!!
+
+    // invoke and verify
+    dao.find(po.k)
+      .test()
+      .assertNext { result ->
+        assertEquals(1, result.size)
+        assertTrue(result.containsKey(po.k))
+        assertEquals(po.v, result[po.k])
+      }
+      .verifyComplete()
+  }
+
+  @Test
+  fun `find single key by fuzzy match`() {
+    // prepare data
+    val prefix = randomString(6)
+    val pos = (1..3).map { repository.save(KeyValuePo(k = "$prefix.$it-${randomKey()}", v = randomValue())).block()!! }
+
+    // invoke and verify
+    dao.find("$prefix.%")
+      .test()
+      .assertNext { result ->
+        assertEquals(pos.size, result.size)
+        pos.forEach {
+          assertTrue(result.containsKey(it.k))
+          assertEquals(it.v, result[it.k])
+        }
+      }
+      .verifyComplete()
+  }
+
+  @Test
+  fun `find multiple keys by equation match`() {
+    // prepare data
+    val pos = (1..3).map { repository.save(randomKeyValuePo()).block()!! }
 
     // invoke and verify
     dao.find(*pos.map { it.k }.toTypedArray())
       .test()
       .assertNext { result ->
         assertEquals(pos.size, result.size)
-        pos.forEach { assertEquals(it.v, result[it.k]) }
+        pos.forEach {
+          assertTrue(result.containsKey(it.k))
+          assertEquals(it.v, result[it.k])
+        }
+      }
+      .verifyComplete()
+  }
+
+  @Test
+  fun `find multiple keys by fuzzy match`() {
+    // prepare data
+    val prefix1 = randomString(6)
+    val prefix2 = randomString(6)
+    val pos1 = (1..3).map { repository.save(KeyValuePo(k = "$prefix1.$it-${randomKey()}", v = randomValue())).block()!! }
+    val pos2 = (1..3).map { repository.save(KeyValuePo(k = "$prefix2.$it-${randomKey()}", v = randomValue())).block()!! }
+
+    // invoke and verify
+    dao.find("$prefix1.%", "$prefix2.%")
+      .test()
+      .assertNext { result ->
+        assertEquals(pos1.size + pos2.size, result.size)
+        pos1.forEach {
+          assertTrue(result.containsKey(it.k))
+          assertEquals(it.v, result[it.k])
+        }
+        pos2.forEach {
+          assertTrue(result.containsKey(it.k))
+          assertEquals(it.v, result[it.k])
+        }
+      }
+      .verifyComplete()
+  }
+
+  @Test
+  fun `find multiple keys by equation match and fuzzy match`() {
+    // prepare data
+    val prefix1 = randomString(6)
+    val prefix2 = randomString(6)
+    repository.save(randomKeyValuePo()).block()!!
+    val po1 = repository.save(randomKeyValuePo()).block()!!
+    val po2 = repository.save(KeyValuePo(k = "$prefix1.1-${randomKey()}", v = randomValue())).block()!!
+    val po3 = repository.save(KeyValuePo(k = "$prefix1.2-${randomKey()}", v = randomValue())).block()!!
+    val po4 = repository.save(KeyValuePo(k = "$prefix2.1-${randomKey()}", v = randomValue())).block()!!
+    val po5 = repository.save(KeyValuePo(k = "$prefix2.2-${randomKey()}", v = randomValue())).block()!!
+
+    // invoke and verify
+    dao.find(po1.k, "$prefix1.%", "$prefix2.%")
+      .test()
+      .assertNext { result ->
+        assertEquals(5, result.size)
+        assertTrue(result.containsKey(po1.k))
+        assertEquals(po1.v, result[po1.k])
+
+        assertTrue(result.containsKey(po2.k))
+        assertEquals(po2.v, result[po2.k])
+
+        assertTrue(result.containsKey(po3.k))
+        assertEquals(po3.v, result[po3.k])
+
+        assertTrue(result.containsKey(po4.k))
+        assertEquals(po4.v, result[po4.k])
+
+        assertTrue(result.containsKey(po5.k))
+        assertEquals(po5.v, result[po5.k])
       }
       .verifyComplete()
   }
